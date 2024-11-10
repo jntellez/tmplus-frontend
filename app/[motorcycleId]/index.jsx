fda51f696e586152cf4b1322a78106c7a8e5659a
Juan Tellez <juantellez916@gmail.com>
2024-11-10 15:01:46 -0600

    swiper and modal added on motorcycle detail

--- app/[motorcycleId]/index.jsx
+++ app/[motorcycleId]/index.jsx
@@ -1,16 +1,9 @@
 import React, { useEffect, useState } from "react";
-import {
-  View,
-  Text,
-  Button,
-  StyleSheet,
-  Image,
-  ScrollView,
-} from "react-native";
+import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
 import colors from "../../theme/colors";
 import { router, useLocalSearchParams } from "expo-router";
 import { getMotorcycleById } from "../../services/motorcycleService";
-import motorcycleImg from "../../assets/motorcycleImg.png";
+import MotorcycleSwiper from "../../components/MotorcycleSwiper"; // Importamos el nuevo componente
 
 const DetailScreen = () => {
   const { motorcycleId } = useLocalSearchParams();
@@ -29,12 +22,8 @@ const DetailScreen = () => {
 
   return (
     <ScrollView style={styles.container}>
-      <Image
-        source={motorcycle.image ? { uri: motorcycle.image } : motorcycleImg} // Imagen de la moto o imagen de fallback
-        style={styles.image}
-        resizeMode="cover" // Ajustar el modo de imagen
-      />
-
+      <MotorcycleSwiper images={motorcycle.images} />
+      {/* Usamos el nuevo componente para el Swiper */}
       <View style={styles.card}>
         <Text style={styles.cardHeader}>Detalles de la Motocicleta</Text>
         <View style={styles.cardContent}>
@@ -50,16 +39,12 @@ const DetailScreen = () => {
           <Text style={styles.cardText}>{motorcycle.year}</Text>
         </View>
       </View>
-
-      {/* Tarjeta para Descripción */}
       <View style={styles.card}>
         <Text style={styles.cardHeader}>Descripción</Text>
         <View style={styles.cardContent}>
           <Text style={styles.cardText}>{motorcycle.description}</Text>
         </View>
       </View>
-
-      {/* Fila para Precio de Alquiler y Disponibilidad */}
       <View style={styles.rowCard}>
         <View style={styles.card}>
           <Text style={styles.cardHeader}>Precio de Alquiler</Text>
@@ -68,7 +53,6 @@ const DetailScreen = () => {
           </View>
         </View>
 
-        {/* Tarjeta para Disponibilidad con margen superior */}
         <View style={[styles.card, styles.availabilityCard]}>
           <Text style={styles.cardHeader}>Disponibilidad</Text>
           <View style={styles.cardContent}>
@@ -78,8 +62,6 @@ const DetailScreen = () => {
           </View>
         </View>
       </View>
-
-      {/* Tarjeta para Fecha de Publicación */}
       <View style={styles.card}>
         <Text style={styles.cardHeader}>Fecha de Publicación</Text>
         <View style={styles.cardContent}>
@@ -88,7 +70,6 @@ const DetailScreen = () => {
           </Text>
         </View>
       </View>
-
       {isOwner && (
         <View style={styles.buttonContainer}>
           <Button
@@ -107,7 +88,6 @@ const DetailScreen = () => {
           />
         </View>
       )}
-
       <View style={styles.rentButtonContainer}>
         <Button
           title="Alquilar Motocicleta"
@@ -123,41 +103,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 16,
-    backgroundColor: colors.background, // Fondo oscuro
-  },
-  image: {
-    width: "100%", // Ancho completo
-    height: 200, // Altura de la imagen
-    borderRadius: 10, // Bordes redondeados
-    marginBottom: 16, // Espacio entre imagen y texto
+    backgroundColor: colors.background,
   },
   card: {
-    backgroundColor: colors.cardBackground, // Fondo más claro para las tarjetas
+    backgroundColor: colors.cardBackground,
     borderRadius: 10,
-    padding: 16, // Espaciado interno
-    marginBottom: 16, // Espacio entre tarjetas
-    elevation: 3, // Sombra para dar profundidad
+    padding: 16,
+    marginBottom: 16,
+    elevation: 3,
   },
   cardHeader: {
-    fontSize: 18, // Tamaño de fuente ajustado
+    fontSize: 18,
     fontWeight: "bold",
-    color: colors.linkColor, // Color del encabezado usando colors.linkColor
-    marginBottom: 4, // Espacio debajo del encabezado
+    color: colors.linkColor,
+    marginBottom: 4,
   },
   cardContent: {
-    display: "flex",
     flexDirection: "row",
-    marginBottom: 8, // Espacio entre cada línea
+    marginBottom: 8,
   },
   cardTitle: {
     fontSize: 18,
     fontWeight: "bold",
-    color: colors.primaryTextLight, // Texto claro para el título
-    marginRight: 8, // Espacio a la derecha del título
+    color: colors.primaryTextLight,
+    marginRight: 8,
   },
   cardText: {
     fontSize: 16,
-    color: colors.lightText || "#E0E0E0", // Cambiar a un color más claro y asegurarse de que sea legible
+    color: colors.lightText || "#E0E0E0",
   },
   buttonContainer: {
     marginTop: 16,
@@ -165,9 +138,8 @@ const styles = StyleSheet.create({
     justifyContent: "space-around",
   },
   rowCard: {
-    display: "flex",
     flexDirection: "row",
-    justifyContent: "space-between", // Asegura que haya espacio entre las cards
+    justifyContent: "space-between",
   },
 });
 
---
+++ components/MotorcycleSwiper.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
+import Swiper from "react-native-swiper"; // Importa Swiper
+import motorcycleImg from "../assets/motorcycleImg.png"; // Imagen de fallback
+import { SERVER_URL } from "../constants";
+
+const MotorcycleSwiper = ({ images }) => {
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const swiperKey = images?.length || 0; // Usa el tamaño de las imágenes como "key"
+
+  // Función para abrir el modal con la imagen seleccionada
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setModalVisible(true);
+  };
+
+  // Función para cerrar el modal
+  const closeModal = () => {
+    setModalVisible(false);
+    setSelectedImage(null);
+  };
+
+  return (
+    <View style={styles.swiperContainer}>
+      <Swiper
+        key={swiperKey} // Forzamos una nueva renderización al cambiar el tamaño de las imágenes
+        style={styles.swiper}
+        loop={false}
+        showsPagination={true}
+        paginationStyle={styles.paginationStyle}
+      >
+        {(images && images.length > 0 ? images : [motorcycleImg]).map(
+          (swiperImg, index) => (
+            <TouchableWithoutFeedback
+              key={index}
+              onPress={() => openModal(swiperImg)} // Al presionar la imagen, abrir el modal
+            >
+              <Image
+                source={
+                  swiperImg
+                    ? { uri: `${SERVER_URL}${swiperImg}` }
+                    : motorcycleImg
+                }
+                style={styles.image}
+                resizeMode="cover"
+              />
+            </TouchableWithoutFeedback>
+          )
+        )}
+      </Swiper>
+
+      {/* Modal para imagen a pantalla completa */}
+      <Modal
+        visible={isModalVisible}
+        transparent={true}
+        animationType="fade"
+        onRequestClose={closeModal}
+      >
+        <TouchableOpacity style={styles.modalBackground} onPress={closeModal}>
+          <Image
+            source={{ uri: `${SERVER_URL}${selectedImage}` }}
+            style={styles.modalImage}
+            resizeMode="contain"
+          />
+        </TouchableOpacity>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  swiperContainer: {
+    height: 200,
+    marginBottom: 20,
+  },
+  swiper: {
+    borderRadius: 10,
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+  },
+  paginationStyle: {
+    bottom: 0,
+  },
+  // Estilos para el modal
+  modalBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
+  },
+  modalImage: {
+    width: "100%",
+    height: "80%",
+    borderRadius: 10,
+  },
+});
+
+export default MotorcycleSwiper;
