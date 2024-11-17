78f5e781fbb1927465912dc5573e3d72332a333f
Juan Tellez <juantellez916@gmail.com>
2024-11-16 19:17:38 -0600

    fix show info profile

--- app/profile/index.jsx
+++ app/profile/index.jsx
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { Button, ScrollView, StyleSheet, Text } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from "react-native";
 import LabelProfileCard from "../../components/LabelProfileCard"; // Importamos el componente para las tarjetas
 import ProfileHeader from "../../components/ProfileHeader"; // Importamos el nuevo componente
 import colors from "../../theme/colors"; // Asegúrate de que los colores estén bien definidos en tu tema
@@ -7,14 +13,13 @@ import { logout } from "../../services/authService";
 import { getStorageItem } from "../../services/storageService";
 
 export default function ProfileScreen() {
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const storageUser = await getStorageItem("user");
-        console.log(storageUser); // Para depuración
+        const storageUser = JSON.parse(await getStorageItem("user"));
         setUser(storageUser);
       } catch (error) {
         console.error("Error al obtener los datos del usuario:", error);
@@ -25,9 +30,7 @@ export default function ProfileScreen() {
     fetchUserData();
   }, []);
 
-  if (loading) {
-    return <Text style={styles.loadingText}>Cargando...</Text>;
-  }
+  if (loading) return <ActivityIndicator size="large" color={colors.primary} />;
 
   return (
     <ScrollView style={styles.container}>
@@ -35,7 +38,6 @@ export default function ProfileScreen() {
       <ProfileHeader joinDate={new Date(user.registration_date)} />
 
       {/* Usamos el componente LabelProfileCard para mostrar la información */}
-
       <LabelProfileCard
         label="Nombre"
         value={user.name}
@@ -50,29 +52,30 @@ export default function ProfileScreen() {
       />
       <LabelProfileCard
         label="Contraseña"
-        value="********"
+        value={user.password}
         key="password"
         userId={user.id}
+        noShow
       />
       <LabelProfileCard
         label="Teléfono"
-        value={user.phone || "No registrado"}
+        value={user.phone}
         key="phone"
         userId={user.id}
       />
       <LabelProfileCard
         label="Dirección"
-        value={user.address || "No registrada"}
+        value={user.address}
         key="address"
         userId={user.id}
       />
       <LabelProfileCard
         label="Código de Vendedor"
-        value={user.mp_access_token || "No disponible"}
+        value={user.mp_access_token}
         key="mp_access_token"
         userId={user.id}
+        noShow
       />
-
       {/* Botón de Cerrar Sesión */}
       <Button title="Cerrar Sesión" onPress={logout} color={colors.cancelled} />
     </ScrollView>
